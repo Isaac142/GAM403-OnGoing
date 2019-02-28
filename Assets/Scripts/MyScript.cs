7cf7feaf701c9b6677ffcc655a728221a55cbe9e
Isaac142 <embramaze@gmail.com>
2019-03-01 09:20:46 +1100

    Initial Commit

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    public float playerSpeed;
+    public float speedBurst = 2f;
+    public int ammo = 100;
+    public int health = 100;
+    public float accuracy = 15.5f;
+    public string player = "Isaac";
+    public Vector3 starting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ammo = ammo + 20;
+        Debug.Log("My Ammo Is: " + ammo);
+        transform.position = starting;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerSpeed = playerSpeed + speedBurst;
+
+        transform.Translate(Vector3.up * playerSpeed);
+    }
+}
