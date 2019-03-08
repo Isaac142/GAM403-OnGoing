05c1926b8eb5384335eec5421306395bbcd48c7a
Isaac142 <embramaze@gmail.com>
2019-03-08 11:54:20 +1100

    Change Weapons

---
+++ Assets/Scripts/Loops.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    public GameObject brick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(brick, (new Vector3 (1,1,0)*i), brick.transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
